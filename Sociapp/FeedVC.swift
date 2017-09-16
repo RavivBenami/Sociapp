65635b75893c7b4d236c031a688ba4c103f2aac3
RavivBenami <ravivofficial@gmail.com>
2017-09-16 16:18:30 +0300

    firebase get complete

--- Sociapp/FeedVC.swift
+++ Sociapp/FeedVC.swift
@@ -13,10 +13,28 @@ class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var tbl: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tbl.dataSource = self
         tbl.delegate = self
+        
+        DataService.ds.REF_POSTS.observe(.value, with: {(snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String,AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                        
+                    }
+                }
+            }
+            self.tbl.reloadData()
+        })
+        
     }
 
     @IBAction func signOut(_ sender: UIButton) {
@@ -27,9 +45,11 @@ class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
         performSegue(withIdentifier: "toMainVC", sender: nil)
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("RAVIV: \(post.caption)")
         return tbl.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
